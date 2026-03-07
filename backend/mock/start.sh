9e2a93f7a929b7ac7745455473fa57cfb6e7b10a
Henrique <henriquekkrrr@gmail.com>
2026-03-07 18:26:35 -0300

    atualizar start.sh

--- backend/mock/start.sh
+++ backend/mock/start.sh
@@ -2,7 +2,11 @@
 
 current_path=$(pwd)
 
-npm install json-server@0.17.4 jsonwebtoken cors
+package_file="$current_path/package.json"
+
+if [[ ! -f "$package_file" ]]; then
+    npm install json-server@0.17.4 jsonwebtoken cors
+fi
 
 cp "$current_path/db.seed.json" "$current_path/db.json"
 
