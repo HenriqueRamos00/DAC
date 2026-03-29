cef186473a40ee117ae29728acaae5f3fc3f0699
Henrique <henriquekkrrr@gmail.com>
2026-03-29 12:00:35 -0300

    FET: Dashboard do gerente

---
+++ frontend/app/features/tabela-clientes/tabela-clientes.tsx
@@ -0,0 +1,49 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "~/components/data-table";
+import { Button } from "~/components/ui/button";
+import { getFormattedCurrency } from "~/lib/utils/formatCurrency";
+import type { Cliente } from "~/models/dto/Cliente";
+
+type TabelaClientesProps = {
+  clientes: Cliente[];
+  onAprovar?: (cliente: Cliente) => void;
+  onRejeitar?: (cliente: Cliente) => void;
+};
+
+export function TabelaClientes({
+  clientes,
+  onAprovar,
+  onRejeitar,
+}: TabelaClientesProps) {
+  const columns: ColumnDef<Cliente>[] = [
+    { accessorKey: "nome", header: "Nome" },
+    { accessorKey: "cpf", header: "CPF" },
+    {
+      accessorKey: "salario",
+      header: "Salário",
+      cell: ({ row }) => getFormattedCurrency(row.original.salario),
+    },
+    {
+      id: "acoes",
+      header: () => <div className="w-56 ml-auto text-center">Ações</div>,
+      cell: ({ row }) => (
+        <div className="w-56 ml-auto flex justify-center gap-2">
+          <Button 
+            variant="confirm"
+            className="h-10 min-w-24 px-3"
+            onClick={() => onAprovar?.(row.original)}>
+            Aprovar
+          </Button>
+          <Button 
+            variant="deny"
+            className="h-10 min-w-24 px-3 sm:w-auto" 
+            onClick={() => onRejeitar?.(row.original)}>
+            Rejeitar
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
+  return <DataTable columns={columns} data={clientes} pageSize={5} />;
+}
--- frontend/app/routes/gerente/dashboard.tsx
+++ frontend/app/routes/gerente/dashboard.tsx
@@ -1,5 +1,9 @@
+import { getSessionAutenticada } from "~/services/auth.server";
 import type { Route } from "./+types/dashboard";
 import { AppBreadcrumb } from "~/components/app-breadcrumb";
+import type { Cliente } from "~/models/dto/Cliente";
+import { FileText } from "lucide-react";
+import { TabelaClientes } from "~/features/tabela-clientes/tabela-clientes";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,7 +12,20 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function GerenteDashboard() {
+export async function loader({ request }:Route.LoaderArgs) {
+  const { apiClient } = await getSessionAutenticada(request);
+  const res = await apiClient.get("/clientes");
+
+  if (!res.ok) {
+    throw new Response("Erro ao obter informações do cliente", {status: res.status});
+  }
+
+  const clientes = (await res.json()) as Cliente[];
+  return { clientes };
+}
+
+export default function GerenteDashboard({ loaderData } : Route.ComponentProps) {
+  const { clientes } = loaderData
   return (
     <div className="flex flex-col gap-6">
       <AppBreadcrumb
@@ -17,7 +34,12 @@ export default function GerenteDashboard() {
           { label: "Gerente" },
         ]}
       />
-      <h1 className="text-sm text-primary uppercase">PAINEL DO GERENTE</h1>
+      <div className="flex flex-col bg-card border-3 border-border p-4 gap-4"> 
+        <div className="border-b border-border pb-4"> 
+          <span className="text-primary text-xs uppercase flex items-center gap-2"> Solicitações de Cadastro </span> 
+        </div> 
+            <TabelaClientes clientes={clientes} /> 
+      </div>
     </div>
   );
 }
