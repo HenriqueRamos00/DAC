948de7e81c48329854ce2f2b494aab2dfe94bb00
Henrique <henriquekkrrr@gmail.com>
2026-06-12 21:50:05 -0300

    FET: Logout chama api gateway e 401 redireciona para tela de login

--- frontend/app/routes/logout.tsx
+++ frontend/app/routes/logout.tsx
@@ -1,9 +1,18 @@
 import { destroySession, getSession } from "~/auth/sessions.server";
 import type { Route } from "../+types/root";
 import { redirect } from "react-router";
+import { api } from "~/services/api.server";
 
 export async function action({ request }: Route.ActionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
+
+  try {
+    await api(request).post("/logout");
+  } catch (error) {
+    if (error instanceof Response) {
+      throw error;
+    }
+  }
 
   return redirect("/", {
     headers: {
